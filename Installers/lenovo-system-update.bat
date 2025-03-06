49e4d64d1ba10bfd095dd8b53213aaca4ece8047
erikberndt23 <erik@berndthaus.com>
2025-03-06 15:09:25 -0500

    Added one-liner

--- Installers/lenovo-system-update.bat
+++
@@ -1,5 +0,0 @@
-IF EXIST "%PROGRAMFILES(x86)%\Lenovo\System Update\tvsu.exe" GOTO END
-"\\asti-usa.net\software\system_update_5.08.03.59.exe" /SP- /verysilent /log /norestart
-echo "Lenovo System Update is installed on this computer" > "%PROGRAMFILES(x86)%\Lenovo\System Update\tvsu.exe"
-goto END
-:END
