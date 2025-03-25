5daf993dd8f635d9f1d5c96302791da8b527ce10
erikberndt23 <erik@berndthaus.com>
2025-03-25 17:11:08 -0400

    added one liner

--- Active Directory/AD-User-Last-Password-Change.ps1
+++ Active Directory/AD-User-Last-Password-Change.ps1
@@ -1,3 +1,7 @@
-﻿Get-ADUser -filter * -searchbase "OU=Executive,OU=Superior Paving Employees,DC=superiorpaving,DC=net" -properties passwordlastset, passwordneverexpires, CanonicalName | sort-object name, CanonicalName | select-object Name, CanonicalName, passwordlastset, passwordneverexpires | Export-csv -path c:\temp\Executive-user-password-info-20190531.csv
+﻿# Export to CSV
+Get-ADUser -filter * -searchbase "OU='OU TO SEARCH',DC='asti-usa',DC='net'" -properties passwordlastset, passwordneverexpires, CanonicalName | sort-object name, CanonicalName | select-object Name, CanonicalName, passwordlastset, passwordneverexpires | Export-csv -path $env:temp\user-password-change-info.csv
 Start-Sleep -s 5
-Get-ChildItem -Filter C:\temp\*.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv c:\temp\merged.csv -NoTypeInformation -Append
+Get-ChildItem -Filter $env:temp\user-password-change-info.csv| Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv $env:temp\merged.csv -NoTypeInformation -Append
+
+# One Liner
+Get-ADUser -filter * -properties PasswordLastSet | Sort-Object Name | Format-Table Name, PasswordLastSet
