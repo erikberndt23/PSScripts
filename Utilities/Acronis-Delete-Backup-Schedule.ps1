4e27a2c7b1c327cbd278e9a5c460edc7cc508ed9
Erik B <berndt.erik@gmail.com>
2025-05-10 09:45:43 -0400

    Update Acronis-Delete-Backup-Schedule.ps1

--- Utilities/Acronis-Delete-Backup-Schedule.ps1
+++ Utilities/Acronis-Delete-Backup-Schedule.ps1
@@ -1,5 +1,7 @@
 $app = Get-Package -Name "*Acronis True Image*"
-$schedTool = "C:\Users\erikb\Documents\Tools\schedmgr.exe"
+$downloadUrl = https://acronis.file.force.com/sfc/dist/version/download/?oid=00D300000000Zcb&ids=0681T00000OfiwU&d=%2Fa%2F1T000001CLaM%2F8B5s7QKcRagXdnK43bnex.L8MheUeRJg0aj0OOZC8sE&asPdf=false
+$downloadLocation = $env:temp
+$schedTool = "$downloadLocation/schedmgr.exe"
 $schedArgs = @(
     "task zap"
 )
@@ -7,7 +9,12 @@ $schedArgs = @(
 # Check if Acronis True Image is Installed
 if ($app.Name -contains "Acronis True Image") {
     Write-Host "Acronis True Image is installed - removing backup schedule!"
-    Start-Process -FilePath $schedTool -ArgumentList $schedArgs
+
+# Download Acronis Schedule Manager
+Invoke-WebRequest -Url $downloadUrl -outFile $downloadLocation/schedmgr.exe
+
+# Run Schedule Manager and disable all backup jobs
+Start-Process -FilePath $schedTool -ArgumentList $schedArgs
 }
 Else {
     Write-Host "Acronis True Image is not installed - exiting script!"
