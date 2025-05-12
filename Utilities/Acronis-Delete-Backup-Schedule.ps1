fc4fb72374363f5d5d7d1785eca7780e2d33ebd2
erikberndt23 <erik@berndthaus.com>
2025-05-12 08:26:11 -0400

    Updated Acronis-Delete-Backup-Schedule.ps1

--- Utilities/Acronis-Delete-Backup-Schedule.ps1
+++ Utilities/Acronis-Delete-Backup-Schedule.ps1
@@ -1,32 +1,35 @@
-$app = Get-Package -Name "*Acronis True Image*"
-$downloadUrl = https://acronis.file.force.com/sfc/dist/version/download/?oid=00D300000000Zcb&ids=0681T00000OfiwU&d=%2Fa%2F1T000001CLaM%2F8B5s7QKcRagXdnK43bnex.L8MheUeRJg0aj0OOZC8sE&asPdf=false
+$app = Get-Package -Name "Acronis True Image*" -AllVersions
+$downloadUrl = "https://acronis.file.force.com/sfc/dist/version/download/?oid=00D300000000Zcb&ids=0681T00000OfiwU&d=%2Fa%2F1T000001CLaM%2F8B5s7QKcRagXdnK43bnex.L8MheUeRJg0aj0OOZC8sE&asPdf=false"
 $downloadLocation = $env:temp
 $schedTool = "$downloadLocation\schedmgr.exe"
 $schedManager = "C:\Program Files (x86)\Acronis\BackupAndRecovery\schedmgr.exe"
 $schedArgs = @(
     "task zap"
 )
-# Check if Acronis True Image is Installed
-if ($app.Name -contains "Acronis True Image") {
-    Write-Host "Acronis True Image is installed - removing backup schedule!"
+# Check if Acronis True Image is installed
+$app = Get-Package -Name "Acronis True Image*" -AllVersions -ErrorAction SilentlyContinue
 
-else if (Test-Path -Path $schedManager) {
-    Write-Host "Scheduling tool exists."
-
-Start-Process -FilePath $schedManager -ArgumentList $schedArgs
-}
-
-else if {
-
-Write-Host "scheduling tool does not exist."
-# Download Acronis Schedule Manager
-Invoke-WebRequest -Uri $downloadUrl -outFile $downloadLocation\schedmgr.exe
-}
-
-# Run Schedule Manager and disable all backup jobs
-Start-Process -FilePath $schedTool -ArgumentList $schedArgs
+if ($app) {
+    Write-Host "Acronis True Image is installed - removing scheduled backup jobs!"
+    
+    # Check if schedmgr.exe already exists
+    if (Test-Path -Path $schedManager) {
+        Write-Host "Scheduling tool exists"
+        Start-Process -FilePath $schedManager -ArgumentList $schedArgs -Wait
+    }
+    else {
+        Write-Host "Scheduling tool not found. Downloading.."
+        Invoke-WebRequest -Uri $downloadUrl -OutFile $schedTool
+        if (Test-Path -Path $schedTool) {
+            Start-Process -FilePath $schedTool -ArgumentList $schedArgs -Wait
+        }
+        else {
+            Write-Error "Failed to download schedmgr.exe"
+            Exit 1
+        }
+    }
 }
 else {
     Write-Host "Acronis True Image is not installed - exiting script!"
-    Exit 1
+    Exit 0
 }
---
+++ Utilities/Forticlient-Import-VPN-Profile.ps1
@@ -0,0 +1,8 @@
+﻿# Path to config file containing the IPSec VPN profile
+$profilePath = "\\asti-usa.net\netlogon\duo_sso_import_3.conf"
+# Path to Forticlient executable
+$fortiClient = "C:\Program Files\Fortinet\FortiClient\FCConfig.exe"
+
+# Import VPN profile
+& $fortiClient -m all -f $profilePath -o import -i 1
+Write-Host "ASTi Forticlient IPSec VPN tunnel imported successfully!"
