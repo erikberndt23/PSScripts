81495f322e4ade0cd3217f19aa354aaaf1fba68f
erikberndt23 <erik@berndthaus.com>
2025-06-06 09:51:45 -0400

    added password expiry script

---
+++ Active Directory/Notify-Admins-of-Expiring-Passwords.ps1
@@ -0,0 +1,49 @@
+Import-Module ActiveDirectory
+
+# Configuration
+$DaysThreshold = 14
+$MailFrom = "noreply@asti-usa.com"
+$MailTo = "erik.berndt@asti-usa.com"
+$MailSubject = "ASTi Domain Users with Passwords Expiring in the Next $DaysThreshold Days"
+$SMTPServer = "aspmx.l.google.com"
+
+# Get the current date
+$Today = Get-Date
+
+# Get all enabled users with non-expired passwords
+$Users = Get-ADUser -Filter {Enabled -eq $true -and PasswordNeverExpires -eq $false -and PasswordLastSet -ne $null} -Properties DisplayName, SamAccountName, PasswordLastSet, PasswordNeverExpires, PasswordExpired, msDS-UserPasswordExpiryTimeComputed
+
+# Filter users whose password is expiring within the threshold
+$ExpiringUsers = $Users | Where-Object {
+    $_."msDS-UserPasswordExpiryTimeComputed" -ne $null -and
+    ([datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed") -le $Today.AddDays($DaysThreshold))
+}
+
+# Exit if there are no expiring passwords
+if (-not $ExpiringUsers -or $ExpiringUsers.Count -eq 0) {
+    return
+}
+
+# Prepare the message body
+if ($ExpiringUsers.Count -gt 0) {
+    $MailBody = "The following users have passwords expiring within the next $DaysThreshold days:`n`n"
+    foreach ($user in $ExpiringUsers) {
+        $expiryDate = [datetime]::FromFileTime($user."msDS-UserPasswordExpiryTimeComputed")
+        $MailBody += "User: $($user.SamAccountName) - $($user.DisplayName)`nExpires: $expiryDate`n`n"
+    }
+}
+
+# Create and send the email
+$MailMessage = New-Object System.Net.Mail.MailMessage
+$MailMessage.From = $MailFrom
+$MailMessage.To.Add($MailTo)
+$MailMessage.Subject = $MailSubject
+$MailMessage.Body = $MailBody
+
+$SMTP = New-Object Net.Mail.SmtpClient($SMTPServer)
+try {
+    $SMTP.Send($MailMessage)
+    Write-Output "Notification email sent to domain admins."
+} catch {
+    Write-Error "Failed to send email: $_"
+}
--- Installers/Install-Sigma-BeyondTrust-JumpClient.ps1
+++ Installers/Install-Sigma-BeyondTrust-JumpClient.ps1
@@ -1,11 +1,11 @@
-# Download MSI Installer from BeyondTrust
-invoke-webrequest -URI 'https://sigmadefense.beyondtrustgov.com/download_client_connector?fn=sra-pin-win_x64-j130w8x765yw7gwy8g5f86zd7ziyhx5e8wizc90.msi&jc=f2e5bcdf54fd24c62ba95a1d3ec82f1a&p=winNT-64-msi&ss=2c25ca1d713b3e609090052cc43fd84856aa3468' -OutFile "c:\sra-pin-win_x64-j130w8x765yw7gwy8g5f86zd7ziyhx5e8wizc90.msi"
+# Download MSI Installer
+invoke-webrequest -URI 'https://www.url.com' -OutFile "c:\file.msi"
 
 # Install MSI
 $DataStamp = get-date -Format yyyyMMddTHHmmss
 $logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
 $Arguments = @(
-    "/i" , "c:\sra-pin-win_x64-j130w8x765yw7gwy8g5f86zd7ziyhx5e8wizc90.msi"
+    "/i" , "c:\file.msi"
     ('"{0}"' -f $file.fullname)
     "/qn"
     "/norestart"
@@ -14,4 +14,3 @@ $Arguments = @(
     $logFile
 )
 Start-Process "msiexec.exe" -ArgumentList $Arguments -Wait -NoNewWindow 
-#Start-Process msiexec.exe -Wait -ArgumentList '/I c:\sra-pin-win_x64-j130w8x765yw7gwy8g5f86zd7ziyhx5e8wizc90.msi INSTALLDIR="C:\ProgramData\BeyondTrust-24.2.2" /qn /norestart'
