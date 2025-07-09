5389019c3055fc2772102cfb42f48aaa297df7ae
erikberndt23 <erik@berndthaus.com>
2025-07-09 15:05:35 -0400

    html changes

--- Active Directory/Notify-Users-of-Expiring-Passwords.ps1
+++ Active Directory/Notify-Users-of-Expiring-Passwords.ps1
@@ -4,6 +4,7 @@ Import-Module ActiveDirectory
 $DaysThreshold = 14
 $MailFrom = "noreply@asti-usa.com"
 $MailReplyTo = "itdept@asti-usa.com"
+$MailBCC = "erik.berndt@asti-usa.com"
 $SMTPServer = "aspmx.l.google.com"
 $Today = Get-Date
 
@@ -84,6 +85,7 @@ ASTi IT Department</p>
         $MailMessage.From = $MailFrom
         $MailMessage.To.Add($email)
         $MailMessage.ReplyTo = $MailReplyTo
+        $MailMessage.Bcc = $MailBCC
         $MailMessage.Subject = $MailSubject
         $MailMessage.Body = $MailBody
         $MailMessage.IsBodyHtml = $true
