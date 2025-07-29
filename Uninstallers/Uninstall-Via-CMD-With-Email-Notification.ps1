c217386a4de2061661e21a1bd6e3309911689641
erikberndt23 <erik@berndthaus.com>
2025-07-29 09:23:20 -0400

    added misc comments

--- Uninstallers/Uninstall-Via-CMD-With-Email-Notification.ps1
+++ Uninstallers/Uninstall-Via-CMD-With-Email-Notification.ps1
@@ -1,13 +1,16 @@
 # Software to be uninstalled
+
 $software = "qBackup"
 
 # Registry paths to search
+
 $registryPaths = @(
     'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
     'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
 )
 
 # Email configuration
+
 $smtpServer = "aspmx.l.google.com"
 $mailFrom = "noreply@asti.usa.com"
 $mailTo = "erik.berndt@asti-usa.com"
@@ -16,11 +19,13 @@ $currentUser = $env:USERNAME
 $subject = "Successfully Uninstalled $software on $hostname for $currentUser"
 $messageBody = ""
 
+# Uninstall checks
 
-$found=$false
-$uninstalled=$false
+$found = $false
+$uninstalled = $false
 
 # Scan and uninstall
+
 foreach ($regPath in $registryPaths) {
     Get-ChildItem -Path $regPath -ErrorAction SilentlyContinue | ForEach-Object {
         $displayName = $_.GetValue("DisplayName")
@@ -53,9 +58,11 @@ foreach ($regPath in $registryPaths) {
 }
 
 # Exit script if not found
+
 if (-not $found) {
     Exit 0
 }
 
 # Send email
+
 Send-MailMessage -From $mailFrom -To $mailTo -Subject $subject -Body $messageBody -SmtpServer $smtpServer
