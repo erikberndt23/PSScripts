65f3c40c35fadd055bed90a7868662842429521a
erikberndt23 <erik@berndthaus.com>
2025-08-11 15:24:15 -0400

    Uninstall-via-Get-Package-GUID.ps1

---
+++ Uninstallers/Uninstall-via-Get-Package-GUID.ps1
@@ -0,0 +1,26 @@
+# Application to be uninstalled
+
+$softwareName = "<software_name>"
+
+# Get the MSI GUID for application to be uninstalled
+
+$msi = (Get-Package -ErrorAction SilentlyContinue | Where-Object { $_.Name -like "*$softwareName*" }).FastPackageReference
+
+# Uninstall the package using if the package was found
+
+if ($msi) {
+    Write-Host "$softwareName is installed. Uninstalling now..."
+    Start-Process msiexec.exe -Wait -PassThru -ArgumentList "/x $msi /qn /norestart"
+    Write-Host "$softwareName successfully uninstalled!"
+
+# Post uninstall check
+
+$stillInstalled = Get-Package -ErrorAction SilentlyContinue | Where-Object { $_.Name -like "*$softwareName*" }
+
+if ($stillInstalled) {
+    Write-Host "$softwareName is still installed..."
+}
+
+} else {
+    Write-Host "$softwareName not found...Exiting script!"
+}
