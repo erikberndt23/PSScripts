a843801ac49712be76b00d3c507cb5272da0909e
erikberndt23 <erik@berndthaus.com>
2025-10-01 13:11:46 -0400

    Disable SMB 1.0

---
+++ Utilities/PDQ-Disable-SMB-1.0.ps1
@@ -0,0 +1,17 @@
+﻿# Disable SMB 1.0
+
+Write-Output "Disabling SMB 1.0..."
+
+# Disable SMB1 client driver (LanmanWorkstation dependency)
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters" -Name SMB1 -Value 0 -Force -ErrorAction SilentlyContinue
+
+# Disable SMB1 server
+
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force -ErrorAction SilentlyContinue
+
+# Disable SMB1 optional feature
+
+Disable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol" -NoRestart -ErrorAction SilentlyContinue
+
+Write-Output "SMB 1.0 has been disabled. A reboot may be required to fully apply changes."
